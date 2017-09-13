91491d855a9d56c992a014eb8991173c0a2dd04c
Evan Johnson and Rich Garrick <evan@>
2017-09-13 10:46:14 -0700

    add working parse for initial json

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,10 +1,19 @@
 import { bikeFinder } from "./../js/bike.js";
 
 $(document).ready(function() {
+
+  function displayBikes(bikeArray) {
+    //loop through bike array and display on screen in nice format
+  }
+
   $('#userInputLocation').submit(function(evt) {
     evt.preventDefault();
     let location = $("input[name='userInputLocation']").val();
-    bikeFinder.displayAllBikesByLocation(location);
+    let distance = $("input[name='userInputDistance']").val();
+    $("input").val("");
+
+    bikeFinder.findBikesByLocation(location, distance, displayBikes);
 
   });
+
 });
--- js/bike.js
+++ js/bike.js
@@ -7,16 +7,32 @@
 //user can see a map with stolen bikes in area
 
 export let bikeFinder = {
-  displayAllBikesByLocation: function(location) {
+  findBikesByLocation: function(location, distance, display) {
     $.ajax({
-      url: `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=5&stolenness=proximity`,
+      url: `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=${distance}&stolenness=proximity`,
       type:  "GET",
       data: {
         format: 'json'
       },
       success: function(responseObject) {
-        console.log(responseObject);
+        let ourBikes = [];
+
+       responseObject.bikes.forEach(function(bike) {
+         ourBikes.push(
+           {
+             title: bike.title,
+             year: bike.year,
+             frame_colors: bike.frame_colors,
+             stolen_location: bike.stolen_location,
+             date_stolen: bike.date_stolen
+           }
+         );
+       });
+        console.log('hey');
+        console.log(responseObject.bikes);
+        console.log(ourBikes);
         //call front end function to display information
+        // display(responseObject);
       },
       error: function(error) {
         console.log(error);
